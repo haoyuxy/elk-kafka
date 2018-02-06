1dcfc236c67839b38a376f3978642e347e330d1d
andpay <andpay@SHA1-DMZ12-Openvpn-CU61.(none)>
2018-02-06 18:07:05 +0800

    del gomaxprocs

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"runtime"
 	"github.com/haoyuxy/elk-kafka/filter"
 )
 
 const (
 	MaxCount = 2000  //max xxx count
 )
-func init() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-}
+
 func main() {
 	
 	cfg := filter.Config()
